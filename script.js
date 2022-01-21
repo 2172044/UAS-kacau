a0fc9dfc8860faadec789f8c4022986b844737d9
Kristiani Nainggolan <90815204+2172044@users.noreply.github.com>
2022-01-21 23:36:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
-$.getJSON('https://api.opendota.com/api/heroStats', function(result) {
-    console.log(result);
+$(document).ready(function() {
+    $.getJSON('https://api.opendota.com/api/heroStats', function(result) {
+        let all = result;
+        let semua = ''
+        $.each(all, function(i, data) {
+            // console.log(data.img)
+            //     $('#container1').append(`<div class="card" id="card1">
+            //            <a href="http://" target="_blank" rel="noopener noreferrer"> <img src="https://api.opendota.com` + data.img + `" class="card-img" alt="" srcset="">
+            //         <h5 class="card-title">` + data.localized_name + `</h5>
+
+            //    </a>
+            //     </div>
+            //   `);
+
+            semua += `<div class="card" id="card1">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"> <img src="https://api.opendota.com` + data.img + `" class="card-img" alt="" srcset="">
+                 <h5 class="card-title">` + data.localized_name + `</h5>
+ 
+            </a>
+ 
+ 
+            
+             </div>`
+
+        });
+        $('#container1').html(semua);
+
+
+    });
+    $('.navig').on('click', function() {
+        $('.navig').removeClass('active');
+        $(this).addClass('active');
+
+        let atak = $(this).html();
+        $('h1').html(atak)
+
+        $.getJSON('https://api.opendota.com/api/heroStats', function(result) {
+            let all = result;
+            let sebagian = ''
+            $.each(all, function(i, data) {
+                if (data.attack_type == atak) {
+                    sebagian += `<div class="card" id="card1">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"> <img src="https://api.opendota.com` + data.img + `" class="card-img" alt="" srcset="">
+                 <h5 class="card-title">` + data.localized_name + `</h5>
+ 
+            </a>
+ 
+ 
+            
+             </div>`
+
+                }
+
+            });
+            $('#container1').html(sebagian);
+
+        });
+
+
+    });
+    $('.all').on('click', function() {
+        $.getJSON('https://api.opendota.com/api/heroStats', function(result) {
+            let all = result;
+            let semua = ''
+            $.each(all, function(i, data) {
+                // console.log(data.img)
+                //     $('#container1').append(`<div class="card" id="card1">
+                //            <a href="http://" target="_blank" rel="noopener noreferrer"> <img src="https://api.opendota.com` + data.img + `" class="card-img" alt="" srcset="">
+                //         <h5 class="card-title">` + data.localized_name + `</h5>
+
+                //    </a>
+                //     </div>
+                //   `);
+
+                semua += `<div class="card" id="card1">
+                    <a href="http://" target="_blank" rel="noopener noreferrer"> <img src="https://api.opendota.com` + data.img + `" class="card-img" alt="" srcset="">
+                 <h5 class="card-title">` + data.localized_name + `</h5>
+ 
+            </a>
+ 
+ 
+            
+             </div>`
+
+            });
+            $('#container1').html(semua);
+        })
+
+    })
+
+
 })
