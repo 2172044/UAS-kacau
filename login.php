cd9de0f69f6e7ee959d2f5d5b4787cb57a90be3b
juantuandali <90950227+juantuandali@users.noreply.github.com>
2022-01-22 02:02:59 +0800

    Update and rename login.html to login.php

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,20 +16,22 @@
 
     <div class="container">
         <div class="login">
-            <form action="login-form">
+           <form action="page2.php" method="get">
                 <div class="title">
                     <img src="image/dota-2.png" alt="" srcset="">
                     <h3>DOTA 2</h3>
-
                 </div><br>
-                <div><label for=""> Username</label><br><input type="text"></div><br>
-                <div><label for="">Password</label><br><input type="password" name="" id=""></div><br>
-                <div class="sign"><input type="submit" value="Sign in"></div><br>
+
+                <div><label for="">Username</label><br>
+                    <input type="text" placeholder="Username" name="Username"></div><br>
+                <div><label for="">Password</label><br>
+                    <input type="password" name="Password" id="" placeholder="Password"></div><br>
+                <div class="sign"><button type="submit">Start</button></div><br>
+
+
             </form>
         </div>
     </div>
-
-
 
 </body>
 
